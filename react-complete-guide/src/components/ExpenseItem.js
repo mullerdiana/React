3344ae67986b5ef3d0bd25c2eacc31105d772cff
Diana Muller <dianamuller14@gmail.com>
2022-05-05 18:02:57 -0300

    Alterações em ExpensiveItem

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,7 +1,11 @@
-function ExpensiveItem(){
-    return (
-        <h2>Expense Item</h2>
-    )
+function ExpensiveItem() {
+  return (
+    <>
+      <div>May 5th 2022</div>
+      <div>Car Insurance</div>
+      <div>$294.67</div>
+    </>
+  );
 }
 
 export default ExpensiveItem;
