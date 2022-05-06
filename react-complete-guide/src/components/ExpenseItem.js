8caa5835f59253987121eb267cc870a14b37fd54
Diana Muller <dianamuller14@gmail.com>
2022-05-06 08:39:04 -0300

    Styling component

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
-function ExpensiveItem() {
+import './ExpenseItem.css'
+
+function ExpenseItem() {
     return (
-      <>
+      <div className="expense-item">
         <div>May 5th 2022</div>
-        <div>Car Insurance</div>
-        <div>$294.67</div>
-      </>
+        <div className="expense-item__description">
+          <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+        </div>
+      </div>
     );
   }
   
-  export default ExpensiveItem;
+  export default ExpenseItem;
