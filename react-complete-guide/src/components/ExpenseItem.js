44f2e3716fee5050e5b6e83e5645dc08f6d8e892
Diana Muller <dianamuller14@gmail.com>
2022-05-06 15:27:35 -0300

    props

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
-function ExpensiveItem() {
-  return (
-    <>
-      <div>May 5th 2022</div>
-      <div>Car Insurance</div>
-      <div>$294.67</div>
-    </>
-  );
-}
+import './ExpenseItem.css'
 
-export default ExpensiveItem;
+function ExpenseItem(props) {
+    return (
+      <div className="expense-item">
+        <div>{props.date.toISOString}</div>
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default ExpenseItem;
