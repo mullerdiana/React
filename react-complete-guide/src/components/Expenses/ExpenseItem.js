071c42d749f07348d01ccaf9016e4ab47147ad4d
Diana Muller <dianamuller14@gmail.com>
2022-05-17 15:02:10 -0300

    Including Form input

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -13,7 +13,7 @@ export const ExpenseItem = (props) => {
 
     return (
       <Card className="expense-item">
-        <ExpenseDate date={props.date} />
+        <ExpenseDate date={props.date}/>
         <div className="expense-item__description">
           <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
